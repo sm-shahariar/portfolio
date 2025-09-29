00e0dbb3b6a9355a819d6d3fd81663cb6592fd01
shahariarrahman1 <shahariarsfcit@gmail.com>
2025-09-29 15:39:34 +0600

    Comment out main.js script inclusion in layout

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -76,7 +76,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
+    {{-- <script src="{{ asset('js/main.js') }}"></script> --}}
 
 </body>
 </html>
